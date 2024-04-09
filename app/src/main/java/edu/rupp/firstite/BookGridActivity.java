7f502741fdb0c2f297ff937f0917e13843d5fbea
JackyDom <sopheaoudom8@gmail.com>
2024-04-09 22:24:59 +0700

    first commit of Book Grid

---
+++ app/src/main/java/edu/rupp/firstite/BookGridActivity.java
@@ -0,0 +1,13 @@
+package edu.rupp.firstite;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookGridActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_grid);
+
+    }
+}
--- app/src/main/java/edu/rupp/firstite/HomeFragment.java
+++ app/src/main/java/edu/rupp/firstite/HomeFragment.java
@@ -1,9 +1,12 @@
 package edu.rupp.firstite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
@@ -12,8 +15,41 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // Find the button in the layout
+        Button btnGeneralBook = rootView.findViewById(R.id.btnGenderalBook);
+        Button btnComdyBook = rootView.findViewById(R.id.btnComdyBook);
+        Button btnComicBook = rootView.findViewById(R.id.btnComicBook);
 
+        // Set OnClickListener to the button Genenral-Book
+        btnGeneralBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the MainActivity
+                Intent intent = new Intent(getActivity(), BookGridActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set OnClickListener to the button Comdy-Book
+        btnComdyBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentComdyBook = new Intent(getActivity(), BookGridActivity.class);
+                startActivity(intentComdyBook);
+            }
+        });
+
+        // Set OnClickListener to the button Comic-Book
+        btnComicBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentComicBook = new Intent(getActivity(), BookGridActivity.class);
+                startActivity(intentComicBook);
+            }
+        });
+
+        return rootView;
     }
 }
