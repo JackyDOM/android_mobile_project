b39e6641b8c1f6b67cc1037fe071fece4d9ba2e6
JackyDom <sopheaoudom8@gmail.com>
2024-04-10 22:00:47 +0700

    first commit of BookDetailActivity

---
+++ app/src/main/java/edu/rupp/firstite/BookDetailActivity.java
@@ -0,0 +1,15 @@
+package edu.rupp.firstite;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+    }
+}
--- app/src/main/java/edu/rupp/firstite/BookGridActivity.java
+++ app/src/main/java/edu/rupp/firstite/BookGridActivity.java
@@ -1,6 +1,9 @@
 package edu.rupp.firstite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BookGridActivity extends AppCompatActivity {
@@ -8,6 +11,12 @@ public class BookGridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_grid);
+    }
 
+    // Method to handle click event on the ImageView
+    public void onImageClick(View view){
+        // Start the new activity using an Intent
+        Intent intent = new Intent(this, BookDetailActivity.class);
+        startActivity(intent);
     }
 }
