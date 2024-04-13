e46dc8448d9b254c9b6b4751b85c36c657ea12ad
JackyDom <sopheaoudom8@gmail.com>
2024-04-13 13:50:45 +0700

    first commit of signIn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,7 @@ android {
 
 dependencies {
 
-    implementation("com.android.volley:volley:1.2.1")
-    //noinspection GradleCompatible,GradleCompatible
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    //implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
@@ -43,12 +41,10 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.picasso:picasso:2.71828")
-//    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.10")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("androidx.cardview:cardview:1.0.0")
-
-
 
 }
--- app/src/main/java/edu/rupp/firstite/MainActivity.java
+++ app/src/main/java/edu/rupp/firstite/MainActivity.java
@@ -1,22 +1,40 @@
 package edu.rupp.firstite;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.btnSignIn);
+        buttonSignIn = (Button) findViewById(R.id.btnSignIn);
+        button = (Button) findViewById(R.id.btnSignUp);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { authenticateUser(); }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,8 +43,79 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openActivitySignUp(){
+    public void authenticateUser() {
+        EditText edtUsername = findViewById(R.id.editTextEmail);
+        EditText edtPassword = findViewById(R.id.editTextPassword);
+
+        String username = edtUsername.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            // Show a toast message indicating that both fields are required
+            Toast.makeText(MainActivity.this, "Username or password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AuthRequest request = new AuthRequest(username, password);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:5000/auth/login")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Call<AuthResponse> call = apiService.login(request);
+        call.enqueue(new Callback<AuthResponse>() {
+            @Override
+            public void onResponse(Call<AuthResponse> call, Response<AuthResponse> response) {
+                if (response.isSuccessful()) {
+                    AuthResponse authResponse = response.body();
+                    String accessToken = authResponse.getAccessToken();
+                    Toast.makeText(MainActivity.this, "Sign-in successful", Toast.LENGTH_SHORT).show();
+                    System.out.println("Access Token: " + accessToken);
+
+                    // Replace the current fragment with HomeFragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentHome, new HomeFragment())
+                            .commit();
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed to sign in", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AuthResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failed to sign in", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void openActivitySignUp() {
         Intent intent = new Intent(this, MainActivitySignUp.class);
         startActivity(intent);
+    }
+
+    public static class AuthRequest {
+        private String username;
+        private String password;
+
+        public AuthRequest(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    public static class AuthResponse {
+        private String access_token;
+
+        public String getAccessToken() {
+            return access_token;
+        }
+    }
+
+    public interface ApiService {
+        @POST("http://10.0.2.2:5000/auth/login")
+        Call<AuthResponse> login(@Body AuthRequest request);
     }
 }
