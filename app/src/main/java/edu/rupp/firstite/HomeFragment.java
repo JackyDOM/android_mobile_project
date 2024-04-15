b811e37b21aeb4f7ed5f62489909f6c87e40f44c
JackyDom <sopheaoudom8@gmail.com>
2024-04-15 20:30:07 +0700

    first commit of access_token in Homefragment

--- app/src/main/java/edu/rupp/firstite/HomeFragment.java
+++ app/src/main/java/edu/rupp/firstite/HomeFragment.java
@@ -1,7 +1,11 @@
 package edu.rupp.firstite;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +15,24 @@ import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
+    private String accessToken;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Retrieve the access token from SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        accessToken = sharedPreferences.getString("access_token", null);
+
+// Log the retrieved access token
+        Log.d("AccessToken", "Retrieved Access Token: " + accessToken);
+
+// Store the access token for later use
+        storeAccessTokenForLaterUse(accessToken);
+
 
         // Find the button in the layout
         Button btnGeneralBook = rootView.findViewById(R.id.btnGenderalBook);
@@ -52,4 +69,15 @@ public class HomeFragment extends Fragment {
 
         return rootView;
     }
+
+    private void storeAccessTokenForLaterUse(String accessToken) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("access_token_home", accessToken);
+        editor.apply();
+
+        // Log the stored access token
+        Log.d("AccessToken", "Stored Access Token: " + accessToken);
+    }
+
 }
