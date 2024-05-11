2ead3726a99aaad158c891cba07e35fbb6097d2f
wayha1 <vehaseng1122@gmail.com>
2024-05-11 17:45:53 +0700

    successful updating 1234578

--- app/src/main/java/edu/rupp/firstite/Home_screen/BookGridActivity.java
+++ app/src/main/java/edu/rupp/firstite/Home_screen/BookGridActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import edu.rupp.firstite.R;
+
 public class BookGridActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_grid);
     }
 }
